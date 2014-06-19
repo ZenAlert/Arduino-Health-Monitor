de3daa215a0d4744a5b73695e4db8f21ec4af46c
Cristiano Falaschi <team@zenalert.com>
2014-06-19 09:06:55 +0200

    Update ArduinoHealthMonitorSerialDebug.ino

--- ArduinoHealthMonitorSerialDebug/ArduinoHealthMonitorSerialDebug.ino
+++ ArduinoHealthMonitorSerialDebug/ArduinoHealthMonitorSerialDebug.ino
@@ -1,5 +1,5 @@
 /*
- Zen Alert Client with Serial Debug 
+ ZenAlert Power and Connection Monitor
  
  This sketch connects to Zen Alert Service
  using a Arduino Ethernet shield or Arduino Ethernet.
